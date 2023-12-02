3eefcf712577247c3a2b523370fdc9d55db42715
Tomas Baca <klaxalk@gmail.com>
2023-12-02 12:34:18 +0100

    fix

--- .ci/test.sh
+++ .ci/test.sh
@@ -38,7 +38,6 @@ echo "$0: unpack the workspace"
 mv $ARTIFACT_FOLDER/workspace.tar.gz /tmp
 cd /tmp
 tar -xvzf workspace.tar.gz
-source $WORKSPACE/devel/setup.bash
 
 ## | ---------------- clone the tested package ---------------- |
 
@@ -53,10 +52,12 @@ echo "$THIS_TEST_REPOS" | while IFS= read -r REPO; do
   URL=$(echo "$REPO" | awk '{print $2}')
   BRANCH=$(echo "$REPO" | awk '{print $3}')
 
-  [ ! -e $PACKAGE ] && echo "$0: cloning '$URL --depth 1 --branch $BRANCH' into '$PACKAGE'"
-  [ ! -e $PACKAGE ] && git clone $URL --recurse-submodules --shallow-submodules --depth 1 --branch $BRANCH $PACKAGE
+  [ ! -e ${PACKAGE} ] && echo "$0: cloning '$URL --depth 1 --branch $BRANCH' into '$PACKAGE'"
+  [ ! -e ${PACKAGE} ] && git clone $URL --recurse-submodules --shallow-submodules --depth 1 --branch $BRANCH $PACKAGE
 
 done
+
+source $WORKSPACE/devel/setup.bash
 
 echo "$0: installing rosdep dependencies"
 

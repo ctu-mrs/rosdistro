384c17744a7b5cf4aed762e41e4897ca0088a226
Tomas Baca <klaxalk@gmail.com>
2024-12-07 11:26:03 +0100

    stopped weekly tagging of images

--- .ci/build_docker.sh
+++ .ci/build_docker.sh
@@ -13,8 +13,6 @@ cd $MY_PATH
 
 docker login --username klaxalk --password $TOKEN
 
-WEEK_TAG="`date +%Y`_w`date +%V`"
-
 docker buildx create --name container --driver=docker-container --use
 
-docker buildx build . --file Dockerfile --tag ctumrs/mrs_uav_system:latest --tag ctumrs/mrs_uav_system:${WEEK_TAG} --platform=linux/amd64,linux/arm64 --push 
+docker buildx build . --file Dockerfile --tag ctumrs/mrs_uav_system:latest --platform=linux/amd64,linux/arm64 --push 

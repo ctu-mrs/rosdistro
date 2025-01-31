47343f385016fbff773411dcdcbf5f83f0da99b5
Tomas Baca <Tomas Baca>
2025-01-31 16:38:44 +0100

    fox

--- .ci/build.sh
+++ .ci/build.sh
@@ -93,7 +93,7 @@ fi
 PASS_TO_DOCKER_BUILD="Dockerfile artifacts build_script.sh repository"
 
 # this first build compiles the contents of "src" and storest the intermediate
-tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --target stage_build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --file Dockerfile --progress plain
+tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --target stage_build --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --file Dockerfile
 
 echo ""
 echo "$0: exporting the artifacts"
@@ -101,7 +101,7 @@ echo ""
 
 # this second build takes the resulting workspace and storest in in a final image
 # that can be deployed to a drone
-docker build . --target stage_export_artifacts --file Dockerfile --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --output ./output --progress plain
+docker build . --target stage_export_artifacts --file Dockerfile --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --output ./output
 
 echo ""
 echo "$0: updating the base image"
@@ -109,7 +109,7 @@ echo ""
 
 # this second build takes the resulting workspace and storest in in a final image
 # that can be deployed to a drone
-docker build . --no-cache --target stage_update_base --file Dockerfile --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --tag $OUTPUT_IMAGE --progress plain
+docker build . --target stage_update_base --file Dockerfile --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --tag $OUTPUT_IMAGE
 
 cp -r ./output/etc/docker/artifacts/* $ARTIFACTS_FOLDER/
 mv $ARTIFACTS_FOLDER/rosdep.yaml $ARTIFACTS_FOLDER/$ROSDEP_FILE

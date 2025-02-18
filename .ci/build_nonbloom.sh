5fbda8c8051ae60c552f31812d515af5ac57fedc
Tomas Baca <Tomas Baca>
2025-02-18 10:05:40 +0100

    wip nonbloom

--- .ci/build_nonbloom.sh
+++ .ci/build_nonbloom.sh
@@ -9,25 +9,28 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
 LIST=$1
 VARIANT=$2
-PACKAGE_NAME=$3
-WORK_DIR=/tmp/repo
+REPOSITORY=$3
+BASE_IMAGE=$4
+REPOSITORY_PATH=$MY_PATH/docker_builder
+ARTIFACTS_FOLDER=/tmp/artifacts
 ARTIFACTS_FOLDER=/tmp/artifacts
 
 YAML_FILE=${LIST}.yaml
 
 sudo apt-get -y install dpkg-dev
 
-sudo pip3 install -U gitman
-
 ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
 
-REPO=$(./.ci/get_repo_source.py $YAML_FILE $VARIANT $ARCH $PACKAGE_NAME)
+REPO=$(./.ci/get_repo_source.py $YAML_FILE $VARIANT $ARCH $REPOSITORY)
 
-mkdir -p $WORK_DIR
-
-cd $WORK_DIR
+mkdir -p $REPOSITORY_PATH
+cd $REPOSITORY_PATH
 
 PACKAGE=$(echo "$REPO" | awk '{print $1}')
 URL=$(echo "$REPO" | awk '{print $2}')
@@ -35,15 +38,64 @@ BRANCH=$(echo "$REPO" | awk '{print $3}')
 GITMAN=$(echo "$REPO" | awk '{print $4}')
 
 echo "$0: cloning '$URL --depth 1 --branch $BRANCH' into '$PACKAGE'"
-git clone $URL --recurse-submodules --branch $BRANCH $PACKAGE
+git clone $URL --recurse-submodules --branch $BRANCH repository
+
+echo "$0: repository cloned"
 
 cd $WORK_DIR/$PACKAGE/
 
 if [[ "$GITMAN" == "True" ]]; then
+  cd repository
+  pipx install gitman
   [[ -e .gitman.yml || -e .gitman.yaml ]] && gitman install
 fi
 
 cp -r $MY_PATH/../.ci_scripts ./
 
-# call the build script within the clone repository
-./.ci/build_package.sh $VARIANT $ARTIFACTS_FOLDER
+## --------------------------------------------------------------
+## |                        docker build                        |
+## --------------------------------------------------------------
+
+$MY_PATH/wait_for_docker.sh
+
+BUILDER_IMAGE=ctumrs/ros:noetic_builder
+TRANSPORT_IMAGE=alpine:latest
+
+REPOSITORY_PATH=./repository
+ARTIFACTS_PATH=./artifacts
+
+cd $MY_PATH/docker_nonbloom_builder
+
+docker buildx use default
+
+echo "$0: loading cached builder docker image"
+
+docker load -i $ARTIFACTS_FOLDER/builder.tar
+
+echo "$0: image loaded"
+
+[ ! -e artifacts ] && mkdir -p artifacts
+
+cp $ARTIFACTS_FOLDER/base_sha.txt ./artifacts/base_sha.txt
+
+PASS_TO_DOCKER_BUILD="Dockerfile artifacts build_script.sh repository"
+
+echo "$0: running the build in the builder image for $ARCH"
+
+# this first build compiles the contents of "src" and storest the intermediate
+tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --target stage_export_artifacts --build-arg BUILDER_IMAGE=${BUILDER_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --build-arg VARIANT=${VARIANT} --file Dockerfile --output ./cache
+
+# copy the packages that were just built to the docker's artifacts workdir
+# such that the final stage can install them
+cp -r ./cache/etc/docker/artifacts/* ./artifacts
+
+echo "$0: copying artifacts"
+
+# copy the artifacts for the next build job
+cp -r ./cache/etc/docker/artifacts/* $ARTIFACTS_FOLDER/
+mv $ARTIFACTS_FOLDER/rosdep.yaml $ARTIFACTS_FOLDER/$ROSDEP_FILE
+
+echo "$0: "
+echo "$0: artifacts are:"
+
+ls $ARTIFACTS_FOLDER
---
+++ .ci/docker_nonbloom_builder/build_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script will build ROS packages from a particular repository into
+# deb packages
+#
+# INPUT:
+# * ./build.sh <package list file name> <{stable/testing/unstable} variant> <repository name>
+# * /tmp/artifacts containts the build artifacts from the previous jobs
+# * /tmp/artifacts/idx.txt contains idx of the previous job
+# * /tmp/artifacts/generated_***_***.yaml rosdep file from the previous jobs
+
+# OUTPUT:
+# * deb packages are put into the "/tmp/artifacts/$IDX" folder where $IDX is the incremented iterator of this build
+# * /tmp/idx.txt with incremented value of $IDX
+# * /tmp/artifacts/generated_***_***.yaml rosdep file with updated definitions from this build
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+VARIANT=$1
+ARTIFACTS_FOLDER=/etc/docker/artifacts
+
+# call the build script within the clone repository
+/etc/docker/repository/.ci/build_package.sh $VARIANT $ARTIFACTS_FOLDER
